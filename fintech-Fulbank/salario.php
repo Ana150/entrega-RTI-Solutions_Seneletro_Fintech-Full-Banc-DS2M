b835ea7cb65128699eda2dcd858779e2daa2b71a
Ana150 <anaoliveira4267@gmail.com>
2021-08-13 10:45:47 -0300

    Atualização 3

--- fintech-Fulbank/salario.php
+++ fintech-Fulbank/salario.php
@@ -1,5 +1,4 @@
 <?php
-
 if(isset($_GET["nome"]) && isset($_GET["salario"]) 
     && isset($_GET["genero"]) && isset($_GET["cargo"])){
     
@@ -8,6 +7,44 @@ if(isset($_GET["nome"]) && isset($_GET["salario"])
     $genero = $_GET["genero"];
     $cargo = $_GET["cargo"];
 
+    $aumento = 0;
 }else{
     echo "ALGO DEU ERRADO!!!!!";
+    die;
 }
+
+if ($salario > 5000){
+    $aumento = $salario * 1.1;
+}else{
+    $aumento = $salario * 1.2;
+}
+
+if($genero == "f"){
+    echo "A $nome passará a receber R$ $aumento, no cargo de $cargo";
+}elseif($genero == "m"){
+    echo "O $nome passará a receber R$ $aumento, no cargo de $cargo";
+}else{
+    echo "$nome passará a receber R$ $aumento, no cargo de $cargo";
+}
+
+number_format($aumento , 2 , "," , ".");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Aumento</title>
+</head>
+<body>
+    <main>
+        
+    </main>
+</body>
+</html>
+
+
